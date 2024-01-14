10c347ec893f2f508166a502d377f1a90f93d4aa
xransum <xransum@users.noreply.github.com>
2024-01-13 23:22:10 -0500

    Update Python versions in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -13,11 +13,11 @@ from nox_poetry import Session, session
 
 package = "openapi_lookup"
 python_versions = [
-    "3.8",
-    "3.12",
     "3.11",
+    "3.12",
     "3.10",
     "3.9",
+    "3.8",
 ]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
